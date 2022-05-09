61942a47ef7c84c8818c4d70b72793bf7f093228
AB <bera1107@usherbrooke.ca>
2022-05-09 19:59:32 -0400

    Sexy thermo2bin

--- Lers0601-Bera1107/pb_APP_log_comb_E2022/pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin.vhd
+++ Lers0601-Bera1107/pb_APP_log_comb_E2022/pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin.vhd
@@ -53,16 +53,96 @@ component Add4bit
            S : out std_logic_vector(3 downto 0);
            CO : out std_logic);
 end component;
-signal E1, E2, E3, E4 : std_logic ;
+signal E1, E2, E3, E4, Cin, Ctemp, Cout : std_logic;
 signal low_v, mid_v, high_v : std_logic_vector (3 downto 0);
-signal E1_v, E2_v, E3_v, E4_v : std_logic_vector (3 downto 0);
+--signal E1_v, E2_v, E3_v, E4_v : std_logic_vector (3 downto 0); -- it's a tool for later (maybe...)
+signal sumA_v, sumB_v, sumC_v, sumS_v, sumZ_v : std_logic_vector (3 downto 0);
 begin
 low_v <= i_ADC_th(3 downto 0);
 mid_v <= i_ADC_th(7 downto 4);
 high_v <= i_ADC_th(11 downto 8);
+Cin <= '0';
 
 
 
+-- Compte le nombre de bits
+compteur1 : ThermoCompte port map (
+            -- input
+            ADCth(0) => low_v(0),
+            ADCth(1) => low_v(1),
+            ADCth(2) => low_v(2),
+            ADCth(3) => low_v(3),
+            -- output
+            bitQty(0)=> sumA_v(0),
+            bitQty(1)=> sumA_v(1),
+            bitQty(2)=> sumA_v(2),
+            bitQty(3)=> sumA_v(3)
+);
+compteur2 : ThermoCompte port map (
+            -- input
+            ADCth(0) => mid_v(0),
+            ADCth(1) => mid_v(1),
+            ADCth(2) => mid_v(2),
+            ADCth(3) => mid_v(3),
+            -- output
+            bitQty(0)=> sumB_v(0),
+            bitQty(1)=> sumB_v(1),
+            bitQty(2)=> sumB_v(2),
+            bitQty(3)=> sumB_v(3)
+);
+compteur3 : ThermoCompte port map (
+            -- input
+            ADCth(0) => high_v(0),
+            ADCth(1) => high_v(1),
+            ADCth(2) => high_v(2),
+            ADCth(3) => high_v(3),
+            -- output
+            bitQty(0)=> sumC_v(0),
+            bitQty(1)=> sumC_v(1),
+            bitQty(2)=> sumC_v(2),
+            bitQty(3)=> sumC_v(3)
+);
+-- Additionneur
+additionPart1 : Add4bit port map (
+                X(0) => sumA_v(0),
+                X(1) => sumA_v(1),
+                X(2) => sumA_v(2),
+                X(3) => sumA_v(3),
+                
+                Y(0) => sumB_v(0),
+                Y(1) => sumB_v(1),
+                Y(2) => sumB_v(2),
+                Y(3) => sumB_v(3),
+                
+                CI => Cin,
+                
+                S(0) => sumZ_v(0),
+                S(1) => sumZ_v(1),
+                S(2) => sumZ_v(2),
+                S(3) => sumZ_v(3),
+                
+                CO => Ctemp                                
+);
+additionPart2 : Add4bit port map (
+                X(0) => sumZ_v(0),
+                X(1) => sumZ_v(1),
+                X(2) => sumZ_v(2),
+                X(3) => sumZ_v(3),
+                
+                Y(0) => sumC_v(0),
+                Y(1) => sumC_v(1),
+                Y(2) => sumC_v(2),
+                Y(3) => sumC_v(3),
+                
+                CI => Ctemp,
+                
+                S(0) => sumS_v(0),
+                S(1) => sumS_v(1),
+                S(2) => sumS_v(2),
+                S(3) => sumS_v(3),
+                
+                CO => Cout                                
+);                
 -- Verification des erreurs
 eCheck1 : thermoEr port map (
           groupOf4Bits(0) => i_ADC_th(0),
